649ae027660af379a123cb6c96dd49a9be91358d
Wasim Raja <wasim7raja10@gmail.com>
2023-01-11 00:03:17 +0530

    attached store to the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux';
 
 import App from './App';
 import './index.css';
+import reducers from "./reducers";
+
+const store = configureStore(reducers)
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider >
 );
