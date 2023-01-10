1aff05813650f030a28d922a465bd7c3ce8c9fad
Wasim Raja <wasim7raja10@gmail.com>
2023-01-10 23:38:17 +0530

    private route wrapper

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,24 @@
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+  }
   return (
-    <div>
-      <h1>Login</h1>
+    <div className=" flex justify-center flex-col items-center gap-6">
+      <h1 className=" text-blue-400 text-4xl p-8 text-center">Login</h1>
       {/* Login Form with username and password */}
-      <form>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+      <form onSubmit={handleSubmit} className=" form-control w-full max-w-xs space-y-6">
+        <div className=" space-y-2">
+          <label htmlFor="username">Username</label>
+          <input className="input input-bordered w-full max-w-xs" type="text" id="username" name="username" />
+        </div>
 
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-
-
-        <button type="submit">Login</button>
+        <div className=" space-y-2">
+          <label htmlFor="password">Password</label>
+          <input className="input input-bordered w-full max-w-xs" type="password" id="password" name="password" />
+        </div>
+        <button className="btn">Login</button>
       </form>
     </div>
   );
