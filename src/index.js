0f8b51468290d8d0f8d1d883585b55644a126568
Wasim Raja <wasim7raja10@gmail.com>
2023-01-10 23:37:57 +0530

    react 18 render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
-
-import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
