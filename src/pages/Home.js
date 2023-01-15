52f78ac3b105ead61e4f4469e8c5da1af6438926
Wasim Raja <wasim7raja10@gmail.com>
2023-01-15 10:11:01 +0530

    tab button on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import PollCard from "../components/home/PollCard";
 
 const Home = ({ answeredPolls, unansweredPolls, users }) => {
-  
+  const [showPoll, setShowPoll] = useState(0)
   return (
     <div>
       <h1 className=" text-4xl text-center">Dashboard</h1>
-      <h2 className=" text-blue-400 text-2xl underline my-8 text-center">Unanswered Polls</h2>
-      <div className=" flex gap-6 flex-wrap justify-around">
-        {unansweredPolls.map(poll => {
-          return (
-            <PollCard key={poll.id} poll={poll} author={users[poll.author]} />
-          );
-        })}
+      <div className="tabs grid place-content-center w-full p-10">
+        <div>
+          <button onClick={
+            () => setShowPoll(0)
+          } className={`tab tab-bordered ${showPoll === 0 && "tab-active"}`}>Unanswered Poll</button>
+          <button onClick={
+            () => setShowPoll(1)
+          } className={`tab tab-bordered ${showPoll === 1 && "tab-active"}`}>Answered Poll</button>
+        </div>
       </div>
-      <h2 className=" text-blue-400 text-2xl underline my-8 text-center">Answered Polls</h2>
-      <div className=" flex gap-6 flex-wrap justify-around">
-        {answeredPolls.map(poll => {
-          return (
-            <PollCard key={poll.id} poll={poll} author={users[poll.author]} />
-          );
-        })}
-      </div>
+      {
+        showPoll === 0
+          ? <div>
+            <h2 className=" text-blue-400 text-2xl underline my-8 text-center">Unanswered Polls</h2>
+            <div className=" flex gap-6 flex-wrap justify-around">
+              {unansweredPolls.map(poll => {
+                return (
+                  <PollCard key={poll.id} poll={poll} author={users[poll.author]} />
+                );
+              })}
+            </div>
+          </div>
+          : <div>
+            <h2 className=" text-blue-400 text-2xl underline my-8 text-center">Answered Polls</h2>
+            <div className=" flex gap-6 flex-wrap justify-around">
+              {answeredPolls.map(poll => {
+                return (
+                  <PollCard key={poll.id} poll={poll} author={users[poll.author]} />
+                );
+              })}
+            </div>
+          </div>
+      }
+
     </div>
   );
 };
